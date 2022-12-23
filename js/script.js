a8383a8ac5fb5aeeb0e5c8b995a62c72e7cb39be
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-23 21:44:10 +0200

    operate function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+let ans =0;
 function add (num1,num2) {
-	return num1+num2;
+    return num1+num2;
 };
 
 function subtract(num1, num2){
@@ -13,3 +14,20 @@ function multiply(num1 , num2){
 function divide(num1, num2){
     return num1/num2;
 };
+
+function operate(operation, num1, num2){
+    switch (operation) {
+        case '+':
+            ans = add(num1. num2);
+            break;
+        case '-':
+            ans = subtract(num1. num2);
+            break;
+        case '/':
+            ans = divide(num1. num2);
+            break;
+        case '*':
+            ans = multiply(num1. num2);
+            break;
+    }
+}
