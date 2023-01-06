fb6e8ed10f53e1699cf3d16baa7fed1b120c57c2
Howard Mabhugu <hmabhugu.3@gmail.com>
2023-01-06 14:11:34 +0200

    String mathematical operations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let ans = 0;
 let numBfrOp;
 let numAftrOp = 0;
+let numArray = [];
+let opsArray = [];
 let op = "..";
 
 const btn = document.querySelectorAll(".numbers");
@@ -11,19 +13,19 @@ const equal = document.querySelector(".equal");
 
 function add(num1, num2) {
   return parseInt(num1) + parseInt(num2);
-}
+};
 
 function subtract(num1, num2) {
   return num1 - num2;
-}
+};
 
 function multiply(num1, num2) {
   return num1 * num2;
-}
+};
 
 function divide(num1, num2) {
   return num1 / num2;
-}
+};
 
 function operate(operation, num1, num2) {
   let answer = 0;
@@ -42,9 +44,23 @@ function operate(operation, num1, num2) {
       break;
   }
   return answer;
-}
+};
+
 btn.forEach(num => {
   num.addEventListener("click", (e) => {
+    // if(scrnValue.className == "zero"){
+    //   numBfrOp = e.target.textContent;
+    //   screen.textContent =numBfrOp;
+    //   scrnValue.className = "non-zero";
+    // }else{
+    //   if (op == "..") {
+    //     numBfrOp = numBfrOp.concat(e.target.textContent);
+    //     screen.textContent = numBfrOp;
+    //   }else{
+    //     numArray.push(numBfrOp);
+    //     op = "..";
+    //   }
+    // }
     if (scrnValue.className == "zero") {
       numBfrOp = e.target.textContent;
       screen.textContent = numBfrOp;
@@ -56,20 +72,34 @@ btn.forEach(num => {
       } else {
         if (numAftrOp !== 0) {
           numAftrOp = numAftrOp.concat(e.target.textContent);
-          screen.textContent = numBfrOp.concat(op).concat(numAftrOp);
+          screen.textContent = numBfrOp + op + numAftrOp;
+          numArray.push(numAftrOp);
         } else {
           numAftrOp = e.target.textContent;
           screen.textContent = numBfrOp.concat(op).concat(numAftrOp);
         }
       }
     }
+    
   });
 });
 
 
 ops.addEventListener("click", (e) => {
-  op = e.target.textContent;
-  screen.textContent = numBfrOp.concat(op);
+  if(op== ".."){
+    op = e.target.textContent;
+    screen.textContent = numBfrOp.concat(op);
+  }else{
+    console.log("Hona mwanangu")
+    console.log(numBfrOp);
+    numBfrOp = operate(op, numBfrOp, numAftrOp);
+    console.log(numBfrOp);
+    op = e.target.textContent;
+    console.log(op);
+    screen.textContent = numBfrOp + op;
+    numAftrOp = '';
+  }
+
 });
 
 equal.addEventListener("click", () => {
